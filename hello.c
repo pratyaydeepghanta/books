894d6ee2999da49ac8b6e1bc33f729513a9fde64
pratyaydeepghanta <pratyaydeepghanta@gmail.com>
2018-09-03 11:48:27 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,7 @@
+
+
+#include<stdio.h>  //this contains the declaration of the functions which are defined in the standard input output library
+int main()
+{
+	printf("hello world");  //this is a comment
+}
